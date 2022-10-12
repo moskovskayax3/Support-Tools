215a85eb85be3fcbff05c12ad8f5fb9edc225885
moskovskayax3 <60343703+moskovskayax3@users.noreply.github.com>
2022-10-12 13:18:53 +0200

    Init

---
+++ Tools/DownloadTools.ps1
@@ -0,0 +1,51 @@
+<#
+.SYNOPSIS
+Downloads and deploys troubleshooting tools 
+.DESCRIPTION
+Downloads and deploys troubleshooting tools
+.INPUTS
+None required
+.OUTPUTS
+None required
+.NOTES
+  Version:        0.1
+  Author:         PRE
+  Linkedin:       https://www.linkedin.com/in/pascal-reimann-b66698230/
+  Creation Date:  12/10/2022
+  Updated Date:   12/10/2022
+  Purpose/Change: Initial script development
+  Change:         -
+  
+.EXAMPLE
+N/A
+#>
+
+##Create a folder to store everything
+$toolsfolder = "C:\ProgramData\TroubleshootingBox"
+If (Test-Path $toolsfolder) {
+    Write-Output "$toolsfolder exists. Skipping."
+}
+Else {
+    Write-Output "The folder '$toolsfolder' doesn't exist. This folder will be used for storing logs created after the script runs. Creating now."
+    Start-Sleep 1
+    New-Item -Path "$toolsfolder" -ItemType Directory
+    Write-Output "The folder $toolsfolder was successfully created."
+}
+##To install scripts
+set-executionpolicy remotesigned -Force
+
+##Set download locations
+$cmtraceoutput = "C:\ProgramData\TroubleshootingBox\cmtrace.exe"
+
+##Force install NuGet (no popups)
+install-packageprovider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
+##Force install Autopilot Diagnostics (no popups)
+Install-Script -Name Get-AutopilotDiagnostics -Force
+
+##Download CMTrace
+Invoke-WebRequest `
+-Uri "https://github.com/andrew-s-taylor/public/raw/main/Troubleshooting/CMTrace.exe" `
+-OutFile $cmtraceoutput `
+-UseBasicParsing `
+-Headers @{"Cache-Control"="no-cache"}
